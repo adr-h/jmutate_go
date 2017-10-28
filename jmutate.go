a570bc6c5c42eae8bf66dd8c323e67fe58e283de
Adrian <6987180+adr-h@users.noreply.github.com>
2017-10-29 01:35:47 +0800

    create a quick-and-dirty Apply function

--- jmutate.go
+++ jmutate.go
@@ -20,7 +20,7 @@ func New(mutationDocument []byte) (JsonMutation, error){
 
 	for key, value := range jsonMap {
 		pointer, err := jpointer.NewJsonPointer(key)
-		if err != nil{
+		if err != nil {
 			return mutation, err
 		}
 		mutation.operations[pointer] = value
@@ -30,12 +30,27 @@ func New(mutationDocument []byte) (JsonMutation, error){
 }
 
 
-//func (j JsonMutation) Apply(document []byte) (newDocument []byte, err error) {
-//	for pointer, mutationDesc := range j.changes {
-//		val, kind, err := pointer.Get(document)
-//		if (err != nil){
-//			return
-//		}
-//	}
-//
-//}
+func (j JsonMutation) Apply(document []byte) (newDocument []byte, err error) {
+	newDocument = make([]byte, len(document))
+	copy(newDocument, document)
+
+	for pointer, operationDoc := range j.operations {
+		operationReceiver, _, err := pointer.Get(document)
+		if (err != nil) {
+			return newDocument, err
+		}
+
+		operationResult, err := operationDoc.Run(operationReceiver)
+		if (err != nil){
+			return newDocument, err
+		}
+
+		setResult, err := pointer.Set(document, operationResult)
+		if (err != nil) {
+			return newDocument, err
+		}
+		newDocument, _ = setResult.([]byte)
+	}
+
+	return newDocument, nil
+}
