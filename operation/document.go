b4ab7230ba1f31cf4a9abc3f9bd615950a0a2784
Adrian <6987180+adr-h@users.noreply.github.com>
2017-10-29 01:11:02 +0800

    remove unused constructor and Validate function; create a new Run function that will attempt to run the migration described in the operation document.

--- operation/document.go
+++ operation/document.go
@@ -7,19 +7,11 @@ type Document struct {
 	Operation `json:"-"`
 }
 
-func NewDocument(op string, arg interface{}) (doc Document, err error) {
-	doc.op = op
-	doc.arg = arg
-	err = doc.Validate()
-	return doc, err
-}
-
-func (d Document) Validate() error{
-	op, err := OperationFactory(d.op, d.arg)
-	if(err != nil){
-		return err
+func (d Document) Run(receiver interface{}) (result interface{},err error){
+	operation, err := OperationFactory(d.op, d.arg)
+	if (err != nil){
+		return
 	}
 
-	d.Operation = op
-	return nil
+	return operation.Apply(receiver)
 }
