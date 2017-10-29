f10fbe38c2d794be53e48502711883aaff7af226
Adrian <6987180+adr-h@users.noreply.github.com>
2017-10-29 16:49:00 +0800

    update the unit test for Incr to reflect the recent changes on it. Also added more test cases

--- operation/incr/incr_test.go
+++ operation/incr/incr_test.go
@@ -6,81 +6,171 @@ import (
 )
 
 func TestIncr(t *testing.T){
-	Convey("Given a valid int argument (6)", t, func(){
-		argument := 6
+	Convey("Given a valid constructor parameter (int, 6)", t, func(){
+		constructorParam := 6
 
 		Convey("Creating a new Incr struct should work", func (){
-			incrStruct, err := New(argument)
+			incrStruct, err := New(constructorParam)
 			So(err, ShouldBeNil)
 
-			Convey("Given a valid int receiver (11)" , func (){
-				receiver := 11
+			Convey("Given a document with a valid value (int, 11)" , func (){
+				document := make(map[string]interface{})
+				document["years"] = 11
 
-				Convey("Applying should work, and the result should be as expected (an int of value 17)", func () {
-					result, err := incrStruct.Apply(receiver)
-					So(err, ShouldBeNil)
-					So(result,ShouldEqual,17)
+				Convey("Given a valid jsonpointer", func (){
+					pointer := "/years"
+
+					Convey("Applying should work, and the result should be as expected (an int of value 17)", func () {
+						result, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldBeNil)
+						So(result["years"],ShouldEqual,17)
+					})
+				})
+
+				Convey("Given an invalid valid jsonpointer", func (){
+					pointer := "/tomatosauce"
+
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
 				})
 			})
 
-			Convey ("Given a valid float receiver (3.14)", func (){
-				receiver := 3.14
+			Convey ("Given a document with a valid value (float, 3.14)", func (){
+				document := make(map[string]interface{})
+				document["years"] = 3.14
 
-				Convey("Applying should work, and the result should be as expected (an int of value 9.14)", func () {
-					result, err := incrStruct.Apply(receiver)
-					So(err, ShouldBeNil)
-					So(result,ShouldEqual,9.14)
+				Convey("Given a valid jsonpointer", func (){
+					pointer := "/years"
+					Convey("Applying should work, and the result should be as expected (an int of value 9.14)", func () {
+						result, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldBeNil)
+						So(result["years"],ShouldEqual,9.14)
+					})
+				})
+
+				Convey("Given an invalid valid jsonpointer", func (){
+					pointer := "/tomatosauce"
+
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
 				})
 			})
 
-			Convey("Given an invalid receiver (a string)", func () {
-				receiver := "applesauce"
+			Convey("Given a document with an invalid value (string, 'applesauce')", func () {
+				document := make(map[string]interface{})
+				document["years"] = "applesauce"
+				Convey("Given a valid jsonpointer", func () {
+					pointer := "/years"
 
-				Convey ("Applying should cause an error", func (){
-					_, err := incrStruct.Apply(receiver)
-					So(err, ShouldNotBeNil)
+					Convey ("Applying should cause an error", func (){
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
+				})
+
+				Convey("Given an invalid valid jsonpointer", func (){
+					pointer := "/tomatosauce"
+
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
 				})
 			})
 		})
 	})
 
 
-	Convey("Given a valid float argument (1.45)", t, func(){
-		argument := 1.45
+	Convey("Given a valid constructor parameter (float, 1.45)", t, func(){
+		constructorParam := 1.45
 
 		Convey("Creating a new Incr struct should work", func (){
-			incrStruct, err := New(argument)
+			incrStruct, err := New(constructorParam)
 			So(err, ShouldBeNil)
 
-			Convey("Given a valid int receiver (5)" , func (){
-				receiver := 5
+			Convey("Given a document with a valid value (int, 5)" , func (){
+				document := make(map[string]interface{})
+				document["years"] = 5
 
-				Convey("Applying should work, and the result should be as expected (an int of value 17)", func () {
-					result, err := incrStruct.Apply(receiver)
-					So(err, ShouldBeNil)
-					So(result,ShouldEqual,6.45)
+				Convey("Given a valid pointer", func (){
+					pointer := "/years"
+
+					Convey("Applying should work, and the result should be as expected (a float of value 6.45)", func () {
+						result, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldBeNil)
+						So(result["years"],ShouldEqual,6.45)
+					})
 				})
+
+				Convey("Given an invalid pointer", func (){
+					pointer := "/tomatosauce"
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
+				})
+
+
+
 			})
 
-			Convey ("Given a valid float receiver (12.3)", func (){
-				receiver := 12.3
+			Convey ("Given a document with a valid value (float, 12.3)", func (){
+				document := make(map[string]interface{})
+				document["years"] = 12.3
 
-				Convey("Applying should work, and the result should be as expected (an int of value 9.14)", func () {
-					result, err := incrStruct.Apply(receiver)
-					So(err, ShouldBeNil)
-					So(result,ShouldEqual,13.75)
+				Convey("Given a valid pointer", func (){
+					pointer := "/years"
+					Convey("Applying should work, and the result should be as expected (an int of value 9.14)", func () {
+						result, err := incrStruct.Apply(pointer, document)
+						So(err, ShouldBeNil)
+						So(result["years"],ShouldEqual,13.75)
+					})
 				})
+
+				Convey("Given an invalid pointer", func (){
+					pointer := "/tomatosauce"
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
+				})
+
 			})
 
-			Convey("Given an invalid receiver (a string)", func () {
-				receiver := "applesauce"
+			Convey("Given a document with an invalid value (string, 'applesauce')", func () {
+				document := make(map[string]interface{})
+				document["years"] = "applesauce"
 
-				Convey ("Applying should cause an error", func (){
-					_, err := incrStruct.Apply(receiver)
-					So(err, ShouldNotBeNil)
+				Convey("Given a valid pointer", func (){
+					pointer := "/years"
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
+				})
+
+				Convey("Given an invalid pointer", func (){
+					pointer := "/tomatosauce"
+					Convey("Applying should cause an error", func () {
+						_, err := incrStruct.Apply(pointer,document)
+						So(err, ShouldNotBeNil)
+					})
 				})
 			})
 		})
 	})
 
+
+	Convey("Given an invalid constructor parameter (string, 'Babylon')", t, func (){
+		constructorParam := "Babylon"
+
+		Convey("Creating a new Incr struct should cause an error", func (){
+			_, err := New(constructorParam)
+			So(err, ShouldNotBeNil)
+		})
+	})
 }
